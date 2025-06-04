7130f135c1721de3ea10502dd58b2ea6aab56807
AleksandraPiatek <piatekaleksandra3@gmail.com>
2025-06-04 19:54:25 +0200

    working basic generated tests

--- frontend/src/app/features/passenger/ticket-activate/ticket-activate.component.spec.ts
+++ frontend/src/app/features/passenger/ticket-activate/ticket-activate.component.spec.ts
@@ -1,18 +1,32 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
+import { HeaderComponent } from '../../../shared/components/header.component';
 
-import { TicketActivateComponent } from './ticket-activate.component';
+describe('HeaderComponent', () => {
+  let component: HeaderComponent;
+  let fixture: ComponentFixture<HeaderComponent>;
 
-describe('TicketActivateComponent', () => {
-  let component: TicketActivateComponent;
-  let fixture: ComponentFixture<TicketActivateComponent>;
+  const activatedRouteMock = {
+    params: of({ id: '123' }),
+    queryParams: of({}),
+    snapshot: {
+      paramMap: {
+        get: (key: string) => null,
+      }
+    }
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [TicketActivateComponent]
-    })
-    .compileComponents();
+      imports: [HeaderComponent, HttpClientTestingModule],
+      providers: [
+        { provide: ActivatedRoute, useValue: activatedRouteMock }
+      ]
+    }).compileComponents();
 
-    fixture = TestBed.createComponent(TicketActivateComponent);
+    fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
